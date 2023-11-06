5f148543660d7d6723b18ec30b523bc9adee6162
Emili <emili.garciab@uqvirtual.edu.co>
2023-11-06 09:22:24 -0500

    h

--- src/main/java/co/edu/uniquindio/poo/Enfrentamiento.java
+++ src/main/java/co/edu/uniquindio/poo/Enfrentamiento.java
@@ -1,21 +1,22 @@
 package co.edu.uniquindio.poo;
 
 import java.time.LocalDate;
+import java.util.ArrayList;
 
 public class Enfrentamiento {
     private final String lugar;
     private final LocalDate fechaHora;
     private final Equipo equipos;
-    private final Jueces Juez;
+    private final ArrayList<Integer> juez;
     private final int resultado;
     private final Estados estados;
     
-    public Enfrentamiento(String lugar, LocalDate fechaHora, Equipo equipos, Jueces juez, int resultado,
+    public Enfrentamiento(String lugar, LocalDate fechaHora, Equipo equipos, ArrayList<Integer> juez, int resultado,
             Estados estados) {
         this.lugar = lugar;
         this.fechaHora = fechaHora;
         this.equipos = equipos;
-        Juez = juez;
+        this.juez = juez;
         this.resultado = resultado;
         this.estados = estados;
     }
@@ -32,8 +33,8 @@ public class Enfrentamiento {
         return equipos;
     }
 
-    public Jueces getJuez() {
-        return Juez;
+    public ArrayList<Integer> getJuez() {
+        return juez;
     }
 
     public int getResultado() {
