35cb4083cbf75433237eb4845dd08a73cb4eacc2
Juan Pablo Dios Castro Vanegas <juanp.castrov@uqvirtual.edu.co>
2023-11-20 15:19:14 -0500

    Current

--- src/main/java/co/edu/uniquindio/poo/App.java
+++ src/main/java/co/edu/uniquindio/poo/App.java
@@ -95,6 +95,7 @@ public class App {
 
             current.llenarJuecesTotal();
             current.crearEnfrentamientos();
+            current.mostrarInfoEnfrentamientos();
             current.resolverEnfrentamientos();
 
             decision = Integer.parseInt(JOptionPane.showInputDialog("Oprima 3 para ver los resultados:"));
@@ -104,6 +105,7 @@ public class App {
             if(decision == 3){
 
                 current.ordenarGanadores();
+                
         
                 
 
